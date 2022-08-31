bc0030d5b003071a818f6ad58e3034cec1088c38
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-08-31 13:50:33 +0200

    a lot

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 $('.hamburger-button').click(function(){
-    $('.mobile-menu').fadeToggle(100);
+    $('.mobile-menu').fadeToggle(300);
     $(this).toggleClass('active');
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
