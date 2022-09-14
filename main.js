822a25e6d60fa0488d2305eee1b24f5c79c11c50
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-09-14 10:37:37 +0200

    reset

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-
- // hamburger 
- $('.hamburger-button').click(function(){
-  $('.mobile-menu').fadeToggle(100);
-  $(this).toggleClass('active');
+$('.hamburger-button').click(function(){
+    $('.mobile-menu').fadeToggle(600);
+    $(this).toggleClass('active');
 });
 
-// side nav
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
-  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
