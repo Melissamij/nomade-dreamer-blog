5accad7a3a8f135b75ff12aa0eb8c36302aab0e3
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-09-14 13:59:21 +0200

    menu

--- main.js
+++ main.js
@@ -16,23 +16,12 @@ function openNav() {
     document.body.style.backgroundColor = "white";
   }
 
-  // dropdown
-  /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-title')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+  window.addEventListener("DOMContentLoaded", () => {
+    const link = document.querySelector('[data-menu]');
+    
+    link.addEventListener("click", (e) => {
+      const submenu = e.currentTarget.nextElementSibling
+      submenu.classList.toggle("is-visible");
+    });
+  });
