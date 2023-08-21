e747eee7c86ead1cf61c4d79078f2b76ffd7cb05
Melissamij <102660511+Melissamij@users.noreply.github.com>
2023-08-21 10:19:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ $('.hamburger-button').click(function(){
   $('.dropdown-title').click(function(){
     $(this).next().fadeToggle(600);
     $(this).toggleClass('active');
+    $(".dropdown").not(".active").children(".dropdown-content").slideUp("200");
+    $(".dropdown.active")
+      .children(".dropdown-content").slideDown("200");
 });
 
 window.addEventListener("DOMContentLoaded", () => {
