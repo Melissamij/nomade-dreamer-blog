5d3445a8b6557b3bb844adfd4dc38285f0ca318b
Melissamij <102660511+Melissamij@users.noreply.github.com>
2023-08-21 10:48:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ $('.hamburger-button').click(function(){
 });
 
 // dropdown menu archive
-  $('.dropdown-title').click(function() {
-    $(this).next().fadeToggle(600);
-    $(this).toggleClass('active');
+
+
+  $( document ).ready(function() {
     $(".dropdown-title").click(function(){
-      $(".dropdown-title").removeClass("active");
-    $(".dropdown").not(".active").children(".dropdown-content").slideUp("200");
-    $(".dropdown.active")
-      .children(".dropdown-content").slideDown("200");
-});
-  });
+       $(".dropdown-title").removeClass("active");
+       $(this).addClass("active");
+       $(".dropdown-title").not(".active").children(".dropdown-content").slideUp("200");
+       $(".dropdown-title.active")
+         .children(".dropdown-content").slideDown("200");
+     });
+   });
+
 
 window.addEventListener("DOMContentLoaded", () => {
   const link = document.querySelector('[dropdown-title]');
